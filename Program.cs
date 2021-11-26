55cd45f2764f0eeaa72cdcf7f0d5a7859e41963c
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-26 19:31:20 +0530

    [VIGNESH M] Added: Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.netEmployeePayRollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To ADO.net Employee Pay Roll Service");
+        }
+    }
+}
