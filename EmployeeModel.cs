f64898f32bbf0ec377396db5de8e0096cdeeedb3
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-26 20:26:08 +0530

    [VIGNESH M] Added: EmployeeModel and Employeerepository Class including connection string

---
+++ EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.netEmployeePayRollService
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+
+        public double BasicPay { get; set; }
+        public double Deduction { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public double Netpay { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.netEmployeePayRollService
+{
+    public class EmployeeRepository
+    {
+        public static string connectionString = @"Server=(localdb)\MSSQLLocalDB,Initial Catalog=ADO.net_employee_payroll";
+
+        //Created Connection object with the datebase
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
